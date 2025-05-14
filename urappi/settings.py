7714867e480f6d76a6be67e7a6cfe8937335248b
loonettevillegas <loonette.villegas@ug.uchile.cl>
2025-05-14 13:05:58 -0400

    static mock up repartidor
    static mock up del mock up

--- urappi/settings.py
+++ urappi/settings.py
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.0.14']
