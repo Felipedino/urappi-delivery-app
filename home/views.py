02bc194bb4ee9cd062cf350c2fd3aaedfdd70140
Nasha <ignacia.galaz@ug.uchile.cl>
2025-05-25 18:41:19 -0400

    fix: se muestra menu como vista principal

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
+from urappiapp.models import Shop
+
 
 
 def index(request):
-    return render(request, "home/index.html")
+    tiendas = Shop.objects.all()
+    
+    info = {"usuario": {}, "tiendas": tiendas}
+
+    return render(request, 'pedidos/stores_view.html', info)
