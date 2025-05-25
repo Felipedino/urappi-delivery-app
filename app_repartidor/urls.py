0802fb249a5841707a0a7f9c1f68b4e03b0fe7bd
loonettevillegas <loonette.villegas@ug.uchile.cl>
2025-05-25 16:05:49 -0400

    Conexion Backend p
    Terminando conexión backend, estática información y img.

--- app_repartidor/urls.py
+++ app_repartidor/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-import views 
 
 app_name = 'app_repartidor'
 urlpatterns = [
     path('deliverer/', views.repartidor_perfil, name='repartidor_perfil'),
     path('accepted-order/', views.accepted_order, name='accepted_order'),
-    path('order-details/<int:order_id>/', views.order_details, name='order_details'),
+    path('order_selected/', views.order_details, name='order_selected'),
 
 
 ]
--- app_repartidor/views.py
+++ app_repartidor/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render, redirect, get_object_or_404, messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from urappiapp.models import Order, Deliverer , Delivery
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST , require_GET
 
 
 def repartidor_perfil(request):
-    pending_orders = Order.objects.filter(status = 1)
-   
-    return render(request, 'templates/deliverer.html', pending_orders)
+    ##pending_orders = Order.objects.filter(status = 1)
+    pending_orders =[
+        {
+            "orderID": "001",
+            "customer": "Daniel Cáceres",
+            "deliverAt": "B0120",
+            "time": "11:00",
+            "createdAt": "La Sonia",
+            "bill" : "4000CLP",
+            "imageURL":"svg/user1.webp"
+            },
+            {
+            "orderID": "004",
+            "customer": "Juan Pedro",
+            "deliverAt": "Centro Modelamiento Matemático",
+            "time": "15:00",
+            "createdAt": "La Cafeta",
+            "bill" : "8000CLP",
+            "imageURL":"svg/user2.jpg"
+            },
+            {
+            "orderID": "01234",
+            "customer": "Javiera Gonzalez",
+            "deliverAt": "Cancha -3",
+            "time": "19:00",
+            "createdAt": "La Cafeta",
+            "bill" : "2000CLP",
+            "imageURL":"svg/user3.jpg"
+            },
+
+            ]
+    context = {
+        'pending_orders': pending_orders
+    }
+    
+
+
+    return render(request, 'deliverer.html', context)
 
 @require_POST
 def accepted_order(request):
@@ -26,17 +62,47 @@ def accepted_order(request):
 
 
 
-@require_POST
-def order_details(request, order_id):
-    selected_order = get_object_or_404(Order, orderID =order_id)
+@require_GET
+def order_details(request):
+    #selected_order = get_object_or_404(Order, orderID =order_id)
 
     items_list = 0
-    info = {
-        'customer': selected_order.customer_name,
-        'customerID': selected_order.customer_id,
-        'orderID' :selected_order.orderID,
-        'product_list': items_list,
-        'deliverAt': selected_order.deliveredAt,
+    #info = {
+     #   'customer': selected_order.customer_name,
+      #  'customerID': selected_order.customer_id,
+       # 'orderID' :selected_order.orderID,
+        #'product_list': items_list,
+        #'deliverAt': selected_order.deliveredAt,
 
-        }
-    return render(request, 'templates/order_selected.html', info) 
+        #}
+    productos=[ {
+            "ProductName": "Chocolate Trencito",
+            "priceCLP": 3000,
+            "description": "Barra de chocolate de leche",
+            "imageURL":"svg/trencito.avif"
+        },
+        {
+            "ProductName": "Café Capuccino",
+            "priceCLP": 3500,
+            "description": "Café de maquina Marley",
+            "imageURL":"svg/capuccino.jpg"
+
+            
+            }
+        
+        ]
+
+    info ={
+        "orderID":"001",
+            "Status": "Disponible",
+            "customer": "Javiera Gonzalez",
+            "deliverAt": "Cancha -3",
+            "time": "19:00",
+            "createdAt": "La Cafeta",
+            "product_list":productos,
+            "bill": "5000CLP"
+            }
+        
+        
+         
+    return render(request, 'order_selected.html', info) 
