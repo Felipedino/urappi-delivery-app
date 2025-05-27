20f08454135b45ef85357babdb0ca0a2826aa9bf
Nasha <ignacia.galaz@ug.uchile.cl>
2025-05-26 20:00:02 -0400

    fix: se soluciona problema de renderizado del header, queda solucionar el problema del icono del carrito

--- urappiapp/urls.py
+++ urappiapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register', views.register_user, name='register_user'),
     path('login',views.login_user, name='login'),
     path('logout',views.logout_user, name='logout'),
-    path("cart", views.show_cart, name="cart"),
+    
 
 ]
