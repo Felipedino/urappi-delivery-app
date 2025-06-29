27a92a93077e01d9775103287d260b4da506dcc0
Felipe Cardenas <felipeygna.fc@gmail.com>
2025-06-29 16:48:49 -0400

    feat: Implementación de perfil y  funcionalidad de recarga de Upuntos

--- urappiapp/urls.py
+++ urappiapp/urls.py
@@ -1,18 +1,18 @@
-from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('register', views.register_user, name='register_user'),
-    path('login',views.login_user, name='login'),
-    path('logout',views.logout_user, name='logout'),
-   
-    path('comprador/', views.comprador, name='comprador'),
-    path('repartidor/', views.repartidor_perfil, name='repartidor'),  
-    path('vendedor/', views.vendedor, name='vendedor'),
-    
-
+    path("register", views.register_user, name="register_user"),
+    path("login", views.login_user, name="login"),
+    path("logout", views.logout_user, name="logout"),
+    path("comprador/", views.comprador, name="comprador"),
+    path("repartidor/", views.repartidor_perfil, name="repartidor"),
+    path("vendedor/", views.vendedor, name="vendedor"),
+    path("profile/", views.profile, name="profile"),
+    path("recharge_upuntos/", views.recharge_upuntos, name="recharge_upuntos"),
 ]
 
 if settings.DEBUG:
